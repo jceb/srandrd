40b8f8ae91e9dd75d18edb45e5ac72b48f9d9ac4
portix <portix@localhost>
2012-06-16 03:04:48 +0200

    Version bump in PKGBUILD; fixing typo in uninstall rule

---
+++ Makefile
@@ -0,0 +1,54 @@
+#See LICENSE for copyright and license details
+
+TARGET			:= srandrd
+SOURCE			:= srandrd.c
+VERSION			:= 0.1.1
+COPYRIGHT		:= "(C) 2012 Stefan Bolte"
+LICENSE			:= "MIT/X Consortium"
+
+DISTDIR 		:= $(TARGET)-$(VERSION)
+
+PREFIX			?= /usr
+INSTALLDIR	:= $(DESTDIR)$(PREFIX)
+
+MANPREFIX		?= $(PREFIX)/share/man
+MANPREFIX		:= $(DESTDIR)$(MANPREFIX)
+
+CFLAGS		:= -Wall -Os -pedantic -std=c99 #-Werror -Wextra
+CPPFLAGS  += -DNAME=\"$(TARGET)\" -DVERSION=\"$(VERSION)\" 
+CPPFLAGS	+= -DCOPYRIGHT=\"$(COPYRIGHT)\" -DLICENSE=\"$(LICENSE)\"
+LDFLAGS		:= -lX11 -lXrandr
+
+all: $(TARGET)
+
+$(TARGET): $(SOURCE)
+	@echo $(CC) -o $@ $< $(CFLAGS) $(LDFLAGS)
+	@$(CC) -o $@ $< $(CFLAGS) $(LDFLAGS) $(CPPFLAGS)
+
+install: 
+	@echo Installing executable to $(INSTALLDIR)/bin
+	@install -d $(INSTALLDIR)/bin
+	@install -m 755 $(TARGET) $(INSTALLDIR)/bin/
+	@echo Installing manpage to $(MANPREFIX)/man1
+	@install -d $(MANPREFIX)/man1
+	@install -m 644 $(TARGET).1 $(MANPREFIX)/man1
+
+uninstall: 
+	@echo Removing executable from $(INSTALLDIR)/bin
+	@rm -f $(INSTALLDIR)/bin/$(TARGET)
+	@echo Removing manpage from $(INSTALLDIR)/bin
+	@rm -f $(MANPREFIX)/man1/$(TARGET).1
+
+clean: 
+	$(RM) $(TARGET)
+
+doc: srandrd.1
+
+srandrd.1: srandrd.1.txt 
+	a2x --doctype manpage --format manpage $<
+
+dist: 
+	@echo "Creating tarball."
+	@hg archive -t tgz $(DISTDIR).tar.gz
+
+.PHONY: all options clean install uninstall dist
---
+++ srandrd.c
@@ -0,0 +1,103 @@
+/* See LICENSE for copyright and license details
+ * srandrd - simple randr daemon
+ */
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <X11/Xlib.h>
+#include <X11/extensions/Xrandr.h>
+
+static void 
+xerror(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  exit(EXIT_FAILURE);
+}
+static int 
+error_handler() {
+  exit(EXIT_FAILURE);
+}
+static void 
+catch_child(int sig) {
+  (void)sig;
+  while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+static void 
+help(void) {
+  fprintf(stderr, "Usage: "NAME" [option] command\n\n"
+      "Options:\n"
+      "   -h  Print this help and exit\n" 
+      "   -n  Don't fork to background\n" 
+      "   -V  Print version information and exit\n");
+  exit(EXIT_SUCCESS);
+}
+static void 
+version(void) {
+  fprintf(stderr, "    This is : "NAME"\n"
+                  "    Version : "VERSION"\n"
+                  "  Builddate : "__DATE__" "__TIME__"\n"
+                  "  Copyright : "COPYRIGHT"\n"
+                  "    License : "LICENSE"\n");
+  exit(EXIT_SUCCESS);
+}
+int 
+main(int argc, char **argv) {
+  XEvent ev;
+  Display *dpy;
+  int daemonize = 1, args = 1;
+  uid_t uid;
+
+  if (argc < 2) 
+    help();
+  if (*(argv[1]) == '-') {
+    args++; 
+    switch(argv[1][1]) {
+      case 'V' : version();     
+      case 'n' : daemonize = 0; break;
+      default : help(); 
+    }
+  }
+  if (argv[args] == NULL)
+    help();
+
+  if (((uid = getuid()) == 0) || uid != geteuid()) 
+    xerror("%s may not run as root\n", NAME);
+
+  if ((dpy = XOpenDisplay(NULL)) == NULL)
+    xerror("Cannot open display\n");
+
+  if (daemonize) {
+    switch(fork()) {
+      case -1 : xerror("Could not fork\n");
+      case 0  : break;
+      default : exit(EXIT_SUCCESS);
+    }
+    setsid();
+
+    close(STDIN_FILENO);
+    close(STDERR_FILENO);
+    close(STDOUT_FILENO);
+  }
+  signal(SIGCHLD, catch_child);
+
+  XRRSelectInput(dpy, DefaultRootWindow(dpy), RROutputChangeNotifyMask);
+  XSync(dpy, False);
+  XSetIOErrorHandler((XIOErrorHandler) error_handler);
+  while(1) {
+    if (!XNextEvent(dpy, &ev)) {
+      if (fork() == 0) {
+        if (dpy)
+          close(ConnectionNumber(dpy));
+        setsid();
+        execvp(argv[args], &(argv[args]));
+      }
+    }
+  }
+  return EXIT_SUCCESS;
+}
