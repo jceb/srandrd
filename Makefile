50650428c36b2eb1e8d3aa74afe297d31a99f3e3
portix <portix@localhost>
2013-04-21 16:23:18 +0200

    Free screenresource if getting output info fails

---
+++ Makefile
@@ -0,0 +1,56 @@
+#See LICENSE for copyright and license details
+
+TARGET			:= srandrd
+SOURCE			:= srandrd.c
+VERSION			:= 0.3
+COPYRIGHT		:= "(C) 2012-2013 Stefan Bolte"
+LICENSE			:= "MIT/X Consortium"
+
+DISTDIR 		:= $(TARGET)-$(VERSION)
+
+PREFIX			?= /usr
+INSTALLDIR	:= $(DESTDIR)$(PREFIX)
+
+MANPREFIX		?= $(PREFIX)/share/man
+MANPREFIX		:= $(DESTDIR)$(MANPREFIX)
+
+CFLAGS		:= -Wall -Os -pedantic -std=c99 #-Werror -Wextra
+CPPFLAGS  += -D_BSD_SOURCE -D_XOPEN_SOURCE=600
+CPPFLAGS  += -DNAME=\"$(TARGET)\" -DVERSION=\"$(VERSION)\" 
+CPPFLAGS	+= -DCOPYRIGHT=\"$(COPYRIGHT)\" -DLICENSE=\"$(LICENSE)\"
+
+LDFLAGS		:= -lX11 -lXrandr
+
+all: $(TARGET)
+
+$(TARGET): $(SOURCE)
+	@echo $(CC) -o $@ $< $(CFLAGS) $(LDFLAGS)
+	@$(CC) -o $@ $< $(CFLAGS) $(LDFLAGS) $(CPPFLAGS)
+
+install: 
+	@echo Installing executable to $(INSTALLDIR)/bin
+	@install -d $(INSTALLDIR)/bin
+	@install -m 755 $(TARGET) $(INSTALLDIR)/bin/
+	@echo Installing manpage to $(MANPREFIX)/man1
+	@install -d $(MANPREFIX)/man1
+	@install -m 644 $(TARGET).1 $(MANPREFIX)/man1
+
+uninstall: 
+	@echo Removing executable from $(INSTALLDIR)/bin
+	@rm -f $(INSTALLDIR)/bin/$(TARGET)
+	@echo Removing manpage from $(INSTALLDIR)/bin
+	@rm -f $(MANPREFIX)/man1/$(TARGET).1
+
+clean: 
+	$(RM) $(TARGET)
+
+doc: srandrd.1
+
+srandrd.1: srandrd.1.txt 
+	a2x --doctype manpage --format manpage $<
+
+dist: 
+	@echo "Creating tarball."
+	@hg archive -t tgz -X dist $(DISTDIR).tar.gz
+
+.PHONY: all options clean install uninstall dist
---
+++ srandrd.c
@@ -0,0 +1,147 @@
+/* See LICENSE for copyright and license details
+ * srandrd - simple randr daemon
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <X11/Xlib.h>
+#include <X11/extensions/Xrandr.h>
+
+#define OCNE(X) ((XRROutputChangeNotifyEvent*)X)
+#define BUFFER_SIZE 128
+
+char *con_actions[] = { "connected", "disconnected", "unknown" };
+
+static void 
+xerror(const char *format, ...) 
+{
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    exit(EXIT_FAILURE);
+}
+static int 
+error_handler(void) 
+{
+    exit(EXIT_FAILURE);
+}
+static void 
+catch_child(int sig) 
+{
+    (void)sig;
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+static void 
+help(int status) 
+{
+    fprintf(stderr, "Usage: "NAME" [option] command\n\n"
+            "Options:\n"
+            "   -h  Print this help and exit\n" 
+            "   -n  Don't fork to background\n" 
+            "   -V  Print version information and exit\n");
+    exit(status);
+}
+static void 
+version(void) 
+{
+  fprintf(stderr, "    This is : "NAME"\n"
+                  "    Version : "VERSION"\n"
+                  "  Builddate : "__DATE__" "__TIME__"\n"
+                  "  Copyright : "COPYRIGHT"\n"
+                  "    License : "LICENSE"\n");
+  exit(EXIT_SUCCESS);
+}
+int 
+main(int argc, char **argv) 
+{
+    XEvent ev;
+    Display *dpy;
+    int daemonize = 1, args = 1;
+    char buf[BUFFER_SIZE];
+    uid_t uid;
+
+    if (argc < 2) 
+        help(EXIT_FAILURE);
+
+    if (*(argv[1]) == '-') 
+    {
+        args++;
+        switch(argv[1][1]) 
+        {
+            case 'V' : version();     
+            case 'n' : daemonize = 0; break;
+            case 'h' : help(EXIT_SUCCESS);
+            default  : help(EXIT_FAILURE);
+        }
+    }
+    if (argv[args] == NULL)
+        help(EXIT_FAILURE);
+
+    if (((uid = getuid()) == 0) || uid != geteuid()) 
+        xerror("%s may not run as root\n", NAME);
+
+    if ((dpy = XOpenDisplay(NULL)) == NULL)
+        xerror("Cannot open display\n");
+
+    if (daemonize) 
+    {
+        switch(fork()) 
+        {
+            case -1 : xerror("Could not fork\n");
+            case 0  : break;
+            default : exit(EXIT_SUCCESS);
+        }
+        setsid();
+
+        close(STDIN_FILENO);
+        close(STDERR_FILENO);
+        close(STDOUT_FILENO);
+    }
+    signal(SIGCHLD, catch_child);
+
+    XRRSelectInput(dpy, DefaultRootWindow(dpy), RROutputChangeNotifyMask);
+    XSync(dpy, False);
+    XSetIOErrorHandler((XIOErrorHandler) error_handler);
+    while(1) 
+    {
+        if (!XNextEvent(dpy, &ev)) 
+        {
+            XRRScreenResources *resources = XRRGetScreenResources(OCNE(&ev)->display,
+                    OCNE(&ev)->window);
+            if (resources == NULL)
+            {
+                fprintf(stderr, "Could not get screen resources\n");
+                continue;
+            }
+
+            XRROutputInfo *info = XRRGetOutputInfo(OCNE(&ev)->display, resources,
+                    OCNE(&ev)->output);
+            if (info == NULL)
+            {
+                XRRFreeScreenResources(resources);
+                fprintf(stderr, "Could not get output info\n");
+                continue;
+            }
+
+            snprintf(buf, BUFFER_SIZE, "%s %s", info->name,
+                    con_actions[info->connection]);
+            if (fork() == 0) 
+            {
+                if (dpy)
+                    close(ConnectionNumber(dpy));
+                setsid();
+                setenv("SRANDRD_ACTION", buf, False);
+                execvp(argv[args], &(argv[args]));
+            }
+            XRRFreeScreenResources(resources);
+            XRRFreeOutputInfo(info);
+        }
+    }
+    return EXIT_SUCCESS;
+}
